6aea135d206666f1eec60214d257a0be233d5115
liudongdong <liudongdong.2008@outlook.com>
2016-10-08 12:13:50 +0800

    1.添加了Jar包依赖打包 2.添加了多项目功能

---
+++ common/src/main/java/common/test/StringUtil.java
@@ -0,0 +1,11 @@
+package common.test;
+
+/**
+ * Created by liudongdong on 2016/10/8.
+ */
+public class StringUtil {
+    public static boolean isNullOrEmpty(String str)
+    {
+        return  str==null||str.length()==0;
+    }
+}
--- src/main/java/ldd/demo/Application.java
+++ src/main/java/ldd/demo/Application.java
@@ -1,11 +1,14 @@
 package ldd.demo;
 
+import common.test.StringUtil;
+
 /**
  * Created by liudongdong on 2016/10/8.
  */
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello,Gradle");
+        System.out.println(StringUtil.isNullOrEmpty("Hello"));
         try {
             System.in.read();
         } catch (Exception ex) {
